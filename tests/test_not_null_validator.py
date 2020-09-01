bc9468c2e9ed845f5c3c288d35f57a29e373104e
vikassingh1000 <vikassingh1000@outlook.com>
2020-09-02 00:58:15 +0530

    Initial commit

--- tests/test_not_null_validator.py
+++ tests/test_not_null_validator.py
@@ -28,3 +28,98 @@ def test_null_check_custom_msg(spark_session):
 
     assert invalid_df.collect()== invaild_df_expected
     assert valid_df.collect()== vaild_df_expected
+
+
+def test_empty_check_with_custom_excp_clm(spark_session):
+    C_FACT_CLASS_ENTITY_FIELD_TO_VALIDATE_AGAINST = {
+        "a": [NotNullValidator()],
+        "b": [NotNullValidator()],
+    }
+
+
+    test_df = spark_session.createDataFrame([[1, "dummy"], [2, ""]], "a: int, b: string")
+
+    excep_record_handler = DefaultExceptionRecordHandler()
+    val_builder = ValidatorBuilder()
+    data_val = val_builder.add_excp_rec_handler(excep_record_handler).add_validation_map(C_FACT_CLASS_ENTITY_FIELD_TO_VALIDATE_AGAINST).add_validate_rec_df(test_df).build()
+    valid_df,invalid_df = data_val.validate()
+
+    print(invalid_df.collect())
+    invaild_df_expected = [Row(a=2, b='', exception_desc='|b is null |')]
+    vaild_df_expected = [Row(a=1, b='dummy')]
+    assert invalid_df.collect()== invaild_df_expected
+    assert valid_df.collect()== vaild_df_expected
+
+
+def test_empty_check(spark_session):
+    C_FACT_CLASS_ENTITY_FIELD_TO_VALIDATE_AGAINST = {
+        "a": [NotNullValidator()],
+        "b": [NotNullValidator()],
+    }
+
+
+    test_df = spark_session.createDataFrame([[1, "dummy"], [2, ""]], "a: int, b: string")
+
+    excep_record_handler = DefaultExceptionRecordHandler()
+    val_builder = ValidatorBuilder()
+    data_val = val_builder.add_excp_rec_handler(excep_record_handler).add_validation_map(C_FACT_CLASS_ENTITY_FIELD_TO_VALIDATE_AGAINST).add_validate_rec_df(test_df).build()
+    valid_df,invalid_df = data_val.validate()
+
+    print(invalid_df.collect())
+    invaild_df_expected = [Row(a=2, b='', exception_desc='|b is null |')]
+    vaild_df_expected = [Row(a=1, b='dummy')]
+    assert invalid_df.collect()== invaild_df_expected
+    assert valid_df.collect()== vaild_df_expected
+
+def test_null_check(spark_session):
+    C_FACT_CLASS_ENTITY_FIELD_TO_VALIDATE_AGAINST = {
+        "a": [NotNullValidator()],
+        "b": [NotNullValidator()],
+    }
+
+
+    test_df = spark_session.createDataFrame([[1, "dummy"], [2, None]], "a: int, b: string")
+
+    excep_record_handler = DefaultExceptionRecordHandler()
+    val_builder = ValidatorBuilder()
+    data_val = val_builder.add_excp_rec_handler(excep_record_handler).add_validation_map(C_FACT_CLASS_ENTITY_FIELD_TO_VALIDATE_AGAINST).add_validate_rec_df(test_df).build()
+    valid_df,invalid_df = data_val.validate()
+
+    print(invalid_df.collect())
+    invaild_df_expected = [Row(a=2, b=None, exception_desc='|b is null |')]
+    vaild_df_expected = [Row(a=1, b='dummy')]
+
+    assert invalid_df.collect()== invaild_df_expected
+    assert valid_df.collect()== vaild_df_expected
+
+def test_null_check_custom_msg_with_clm_name(spark_session):
+    config  = ExceptionValidatorContext.confg
+
+    custom_msg_lmda_field1 = lambda p_clm, validate_against : sf.concat(sf.col(config.excep_clm_name), sf.lit(f"{p_clm} is empty "),
+                                                                        sf.lit(config.exception_msg_seperator))
+    custom_msg_lmda_field2 = lambda p_clm, validate_against : sf.concat(sf.col(config.excep_clm_name), sf.col("foreign_key"), sf.lit(" is not present in system"),
+                                                                        sf.lit(config.exception_msg_seperator))
+
+    # p_lookup_clm_nme = "foreign_key" ,p_excep_msg = "Foreign key not present in system: "
+    C_FACT_CLASS_ENTITY_FIELD_TO_VALIDATE_AGAINST = {
+        "field1": [NotNullValidator(custom_msg_lmda = custom_msg_lmda_field1)],
+        "field2": [NotNullValidator(custom_msg_lmda = custom_msg_lmda_field2)],
+    }
+
+
+    test_df = spark_session.createDataFrame([[1, "dummy", "foreign_key1"], [None,None,"foreign_key2" ]], "field1: int, field2: string, foreign_key:String")
+
+    excep_record_handler = DefaultExceptionRecordHandler()
+    val_builder = ValidatorBuilder()
+    data_val = val_builder.add_excp_rec_handler(excep_record_handler).add_validation_map(C_FACT_CLASS_ENTITY_FIELD_TO_VALIDATE_AGAINST).add_validate_rec_df(test_df).build()
+    valid_df,invalid_df = data_val.validate()
+    invalid_df.show(10,False)
+    print(invalid_df.collect())
+
+    invaild_df_expected = [Row(a=None, b=None, c="foreign_key2" , exception_desc='|field1 is empty |foreign_key2 is not present in system|')]
+    print(invaild_df_expected)
+    vaild_df_expected = [Row(a=1, b='dummy',foreign_key="foreign_key1")]
+    invaild_row = invalid_df.collect()
+    assert invaild_row == invaild_df_expected
+
+    assert valid_df.collect()== vaild_df_expected
